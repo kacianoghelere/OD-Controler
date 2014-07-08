9a9ece00e9401094c9aac121adc76e7323b4f1a9
Kaciano <kaciano.ghelere@serhmatica.com.br>
2014-07-08 15:29:57 -0300

    Adicionada origem dos itens

---
+++ src/main/java/br/com/odcontroler/main/view/perk/bean/PerkBean.java
@@ -0,0 +1,66 @@
+package br.com.odcontroler.main.view.perk.bean;
+
+import br.com.odcontroler.data.db.dao.PerkDAO;
+import br.com.odcontroler.data.entity.Perk;
+import br.com.odcontroler.main.object.BeanEvent;
+import br.com.odcontroler.main.view.perk.PerkView;
+import br.com.odcontroler.main.view.bean.ViewBean;
+import br.com.odcontroler.main.view.perk.sub.PerkSubView;
+
+/**
+ * Bean de controle para a PerkView
+ *
+ * @author kaciano
+ */
+public class PerkBean extends ViewBean<PerkView> {
+
+    private PerkDAO dao;
+
+    /**
+     * Cria nova instancia de PerkBean
+     *
+     * @param view <code>PerkView</code> View do Bean
+     */
+    public PerkBean(PerkView view) {
+        super(view);
+        this.dao = new PerkDAO();
+    }
+
+    @Override
+    public void commit(BeanEvent evt) throws Exception {
+        this.dao.replaceAll(getView().getModel().getData());
+    }
+
+    @Override
+    public void add(BeanEvent evt) {
+        Perk perk = (Perk) evt.getValue();
+        getView().getModel().add(perk);
+    }
+
+    @Override
+    public void edit(BeanEvent evt) throws Exception {
+        if (getView().getTable().getSelectedRowCount() > 0) {
+            Integer row = (Integer) getView().getTable().getSelectedRows()[0];
+            PerkSubView subview = new PerkSubView(getView(), getView().getModel().getObject(row));
+            getView().getMainScreen().getListener().insertView(subview);
+            if (subview.getPerk() != null) {
+                getView().getModel().update(row, subview.getPerk());
+            }
+        }
+    }
+
+    /**
+     * Retorna o próximo ID da lista
+     *
+     * @return <code>Long</code> Próximo ID
+     */
+    public Long getNextID() {
+        Long id = (long) 0;
+        for (Perk perk : getView().getModel().getData()) {
+            if (id < perk.getId()) {
+                id = perk.getId();
+            }
+        }
+        return (id + 1);
+    }
+}
---
+++ src/main/java/br/com/odcontroler/main/view/perk/model/PerkModel.java
@@ -0,0 +1,13 @@
+package br.com.odcontroler.main.view.perk.model;
+
+import br.com.gmp.comps.model.GTableModel;
+import br.com.odcontroler.data.entity.Perk;
+
+/**
+ * Modelo da tabela de Perks
+ *
+ * @author kaciano
+ */
+public class PerkModel extends GTableModel<Perk> {
+
+}
