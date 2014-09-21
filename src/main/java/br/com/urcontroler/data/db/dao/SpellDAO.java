8356cbfacca407cb16a414c0074c681c3f2736ff
ka <ka@kaciano-K55A>
2014-09-21 20:15:29 -0300

    Objetos relacionados ao DB4O movidos para projeto externo

--- src/main/java/br/com/urcontroler/data/db/dao/SpellDAO.java
+++ src/main/java/br/com/urcontroler/data/db/dao/SpellDAO.java
@@ -1,6 +1,6 @@
 package br.com.urcontroler.data.db.dao;
 
-import br.com.urcontroler.data.db.GenericDAO;
+import br.com.gmp.comps.data.GenericDAO;
 import br.com.urcontroler.data.entity.Spell;
 
 /**
